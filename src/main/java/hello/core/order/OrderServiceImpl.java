f5dfa4ef207e77adfa6b769b22798e8523d12c62
qwe5507 <apvmffkdls@naver.com>
2021-09-17 18:53:57 +0900

    주문과 할인 도메인 개발 //스프링 핵심 원리 - 기본편

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,20 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId); //우선 멤버 찾고
+        int discountPrice = discountPolicy.discount(member, itemPrice); // member와 price를넘겨 할인 금액을 찾아온다
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
