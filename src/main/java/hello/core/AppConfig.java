6e4a057ccf5cba049177e7fccce94dadcbc4effe
qwe5507 <apvmffkdls@naver.com>
2021-10-21 12:54:18 +0900

    [스프링 핵심 원리 - 기본편] - 스프링 빈 조회 - 기본

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    @Bean(name = "asd")
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
