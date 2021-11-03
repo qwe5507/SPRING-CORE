dedea59eba1f0ab04f7bfef8b1424957e60388fc
qwe5507 <apvmffkdls@naver.com>
2021-11-03 23:15:25 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
+//        basePackages를 지정 하지 않으면 현 클래스의 패키지 경로인 hello.core가 스캔 대상이 된다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
